509cea284b623f9a02a1cdffbe809d46e5c32ff3
Win Feigle <win.feigle@gmail.com>
2022-05-18 20:23:05 -0600

    commit tree 2588a29dc252428c427da810814caee28c1b22d8

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = name => cats.push(name);
+
+const destructivelyPrependCat = name => cats.unshift(name);
+
+const destructivelyRemoveLastCat = () => cats.pop();
+
+const destructivelyRemoveFirstCat = () => cats.shift();
+
+const appendCat = name => {
+    const newCatArr = [...cats, name];
+    return newCatArr;
+}
+
+const prependCat = name => {
+    const newCatArr = [name, ...cats];
+    return newCatArr;
+}
+
+const removeLastCat = () => {
+    const newCatArr = cats.slice(0, (cats.length - 1));
+    return newCatArr;
+}
+
+const removeFirstCat = () => {
+    const newCatArr = cats.slice(1);
+    return newCatArr;
+}
